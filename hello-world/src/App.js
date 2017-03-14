e733a4f57d14ee61c08762c4f1f5442df14f7dae
Don-Draper <shanetanguis@gmail.com>
2017-03-14 11:00:14 -0500

    All buttons working, state set up, ready to move to Node and DB

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -6,67 +6,91 @@ import { Button } from 'reactstrap';
 
 
 
-class Principle extends Component {
+class App extends Component {
 
   constructor (props) {
     super(props);
 
     this.state = {
-      quotes: this.props
+      quotes: {
+        compassionate: ['compassionate 1', 'compassionate 2'],
+        grateful: ['grateful 1', 'grateful 2'],
+        gritty: ['gritty 1', 'gritty']
+      },
+      thisQuote: ''
+      // this.props.quotes.compassionate[0]
     }
 
   }
 
-handleClick(){
-  console.log('button clicked');
-  // this.setState({
+handleClickComp(){
+  console.log('comp button clicked');
+  this.setState({
+    thisQuote: this.state.quotes.compassionate[Math.floor(Math.random()* this.state.quotes.compassionate.length)]
+  });
+};
 
-  // })
-}
+handleClickGrate(){
+  console.log('button');
+  this.setState({
+    thisQuote: this.state.quotes.grateful[Math.floor(Math.random()* this.state.quotes.grateful.length)]
+  });
+};
 
-// handleClickGrateful(){
-//   console.log('button clicked');
-// }
-
-// handleClickGritty(){
-//   console.log('button clicked');
-// }
+handleClickGrit(){
+  console.log('button');
+  this.setState({
+    thisQuote: this.state.quotes.gritty[Math.floor(Math.random()* this.state.quotes.gritty.length)]
+  });
+};
 
   render() {
     return (
       <div className="App">
+        <div className="App-header">
 
-        <Button
-          onClick={this.handleClick.bind(this)}>{this.props.trait}
-        </Button>
+          <h2>What do you want to be?</h2>
+        </div>
 
+          <p></p>
+            <Principle
+              trait={'Compassionate'}
+              handleClick={this.handleClickComp.bind(this)} />
+          <p></p>
+            <Principle
+              trait={'Grateful'}
+              handleClick={this.handleClickGrate.bind(this)} />
+          <p></p>
+            <Principle
+              trait={'Gritty'}
+              handleClick={this.handleClickGrit.bind(this)} />
+          <p></p>
+
+          <h2>Then repeat this three times at every meal...</h2>
+          <p></p>
+
+          <PrincipleList principle={this.state.thisQuote} />
       </div>
     );
   }
 }
 
-var PrincipleList = (props) => (
+var PrincipleList = ({principle}) => (
   <div>
     <ul>
 
-    Eat your cornflakes
+    {principle}
 
     </ul>
   </div>
 );
 
-var App = (props) => (
+var Principle = ({trait, handleClick}) => (
   <div className="App">
-    <div className="App-header">
 
-      <h2>What do you want to be?</h2>
-    </div>
-    {props.traits.map(trait =>
-      <Principle trait={trait} />
-    )}
-
-      <h2>Then repeat these three times over breakfast every day...</h2>
-      <PrincipleList  />
+    <Button
+      onClick={() => {handleClick()}}>{trait}
+    </Button>
 
   </div>
 
